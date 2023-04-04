294c9e5b39fba87b575f3e30f4ff72df3dff79de
Cameron Gordon <camerong888@gmail.com>
2023-04-03 20:27:43 -0400

    Reactive header buttons for phone

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header className="header">
+    <header className={`header-container${menuOpen ? ' open' : ''}`}>
       <h1 className="header-title">Cameron Gordon</h1>
+      <span className="menu-toggle" onClick={handleMenuToggle}>&#9776;</span>
       <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#work-experience">Work Experience</a></li>
-          <li><a href="#contact">Contact</a></li>
+        <ul className="header-nav">
+          <li className="header-nav-item"><a className="header-nav-link" href="#home">Home</a></li>
+          <li className="header-nav-item"><a className="header-nav-link" href="#about">About</a></li>
+          <li className="header-nav-item"><a className="header-nav-link" href="#projects">Projects</a></li>
+          <li className="header-nav-item"><a className="header-nav-link" href="#work-experience">Work Experience</a></li>
+          <li className="header-nav-item"><a className="header-nav-link" href="#contact">Contact</a></li>
         </ul>
       </nav>
     </header>
