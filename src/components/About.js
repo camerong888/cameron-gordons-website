32e853e932b4e08732bc38a75c2476df0f3266db
Cameron Gordon <camerong888@gmail.com>
2023-04-09 15:32:56 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,14 +7,25 @@ function About() {
       <section id="about" className="about">
         <h2 className="about-title">About</h2>
         <p>
-          As a fourth year electrical engineering student at Northeastern
-          University, I am passionate about using technology to improve people's
-          lives. With two internships at companies known for their innovative
-          approaches to engineering, Tesla and Fresenius Medical Care, I have
-          gained valuable experience in the field and developed a strong
-          foundation in electrical engineering principles. I am excited to
-          continue learning and applying my skills to tackle new challenges in
-          the industry.
+          As a senior electrical engineering student at Northeastern University,
+          I am passionate about harnessing technology to improve people's lives.
+          With internships at companies known for their innovative approaches to
+          engineering, Tesla and Fresenius Medical Care, I have acquired
+          valuable hands-on experience and developed a strong foundation in
+          various aspects of electrical engineering, including embedded systems,
+          PCB design, and project ownership.
+        </p>
+        <p>
+          Through these internships and my academic coursework, I have honed my
+          skills in critical thinking, problem-solving, and effective
+          communication. I have also had the opportunity to collaborate with
+          cross-functional teams, learning the importance of teamwork and
+          adaptability in fast-paced environments.
+        </p>
+        <p>
+          As I continue my journey in the field of electrical engineering, I
+          eagerly look forward to expanding my knowledge, embracing new
+          challenges, and contributing to the advancement of technology.
         </p>
       </section>
     </div>
